1ea5104485a6cbe24e9f48d1b3209edb2e45c049
Persimmon <ibrohimsho.a@gmail.com>
2022-03-05 10:43:12 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,4 @@
 #include "Header/pong.h"
-#include "Header/snake.h"
-#include "Header/space_invaders.h"
-#include "Header/tetris.h"
 
 typedef enum {
 	PONG = '1', SNAKE, SPACE_INVADERS, TETRIS
@@ -10,24 +7,11 @@ typedef enum {
 int main() {
 	srand((unsigned int)time(NULL));
 	while(TRUE) {
-		printf( "Press 1 to play Pong."
-				"\nPress 2 to play Snake."
-				"\nPress 3 to play Space Invaders."
-				"\nPress 4 to play Tetris."
-				"\nPress Esc to exit.");
+		printf( "Press 1 to play Pong.\nPress Esc to exit.");
 		char choose_game = getch();
 		switch(choose_game) {
 			case PONG:
 				PlayPong();					// play Pong
-				break;
-			case SNAKE:
-				PlaySnake();				// play Snake
-				break;
-			case SPACE_INVADERS:
-				PlaySpaceInvaders();		// play Space Invaders
-				break;
-			case TETRIS:
-				PlayTetris();				// play Tetris
 				break;
 			case ESCAPE_KEY:
 				return 0;					// exit game
