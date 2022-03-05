2c319c4cd1e87cec7285885098d2c95dcef1ba3f
Persimmon <ibrohimsho.a@gmail.com>
2022-03-05 11:00:42 +0300

    Update common.h

--- Header/common.h
+++ Header/common.h
@@ -25,18 +25,4 @@ void MoveCursorToXY(unsigned short x, unsigned short y) {
 	COORD coord = (COORD){x, y};
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
-
-// prints a loading screen with a progress bar
-void LoadingScreen() {
-	MoveCursorToXY(36, 14);
-	printf("Loading...");
-	MoveCursorToXY(30, 15);
-	unsigned short i;
-	for(i=1;i<=20;i++) {
-		Sleep(100);
-		printf("%c", 178);
-	}
-	system("cls");
-}
-
 #endif
